09f62f4ab5cebb38cc8fae012b9abdd9c63c4856
Christian Suleiman <cisuleiman@gmail.com>
2019-07-09 20:53:02 -0400

    added packages and small aray of friends

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    },
+    {
+        "name":"Chris",
+        "photo":"https://en.wikipedia.org/wiki/Chris_Pratt#/media/File:Chris_Pratt_2018.jpg",
+        "scores":[
+            2,
+            3,
+            1,
+            2,
+            3,
+            1,
+            4,
+            1,
+            2,
+            1
+          ]
+        },
+
+        {
+            "name":"Billy Bob",
+            "photo":"https://en.wikipedia.org/wiki/Billy_Bob_Thornton#/media/File:BillyBobThorntonHWOFFeb2012crop.JPG",
+            "scores":[
+                2,
+                3,
+                1,
+                2,
+                3,
+                1,
+                4,
+                1,
+                2,
+                1
+              ]
+            }
+];
+
+module.exports = friends;
